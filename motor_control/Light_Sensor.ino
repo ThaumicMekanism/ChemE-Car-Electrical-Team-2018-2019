53519a959b22bf41af5127e25f0f00706779c1dd
ThaumicMekanism <thaumicmekanism@gmail.com>
2018-04-05 21:01:59 -0700

    Updated light sensor to reflect digital stuff

--- motor_control/Light_Sensor.ino
+++ motor_control/Light_Sensor.ino
@@ -1,12 +1,12 @@
-bool lightSensor(bool digital) {
-  if (!digital) { //If we are reading the analog values do this.
+bool lightSensor(bool analog) {
+  if (analog) { //If we are reading the analog values do this.
     //Serial.println(analogRead(analogSensor));
     if (analogRead(analogSensor) > 0) {
       return true;
     }
     return false;
   } else { //Else it is digital.
-    return digitalRead(digitalSensor);
+    return !digitalRead(digitalSensor); // The negation is because the digital light sensor we have outputs the inverse.
   }
 }
 
